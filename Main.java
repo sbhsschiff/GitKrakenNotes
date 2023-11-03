8acf41814990048b6368c452b726dcfb9d4e347c
sbhsschiff <43680488+sbhsschiff@users.noreply.github.com>
2023-11-03 08:50:30 -0400

    Output String
    This method will output a string.

--- Main.java
+++ Main.java
@@ -3,6 +3,9 @@ public class Main {
     public static void main(String [] args){
         //This is the main method
     }
-    
+
+    public static void writeString(String str){
+        System.out.println(str);
+    }
     
 }
